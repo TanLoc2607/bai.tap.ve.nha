cde73ea308101e62d27fb80ca29192640ed23049
TânLộc2607 <vckono321@gmail.com>
2023-10-26 00:06:17 +0900

    bai 10

---
+++ bai 10/a.js
@@ -0,0 +1,9 @@
+let a = document.getElementById('1');
+a.innerHTML = "javascript course syllabus";
+
+let b = document.getElementById('2');
+b.innerHTML = "welcome to javascript";
+
+let c = document.getElementById('3');
+c.innerHTML = "javasc";
+
